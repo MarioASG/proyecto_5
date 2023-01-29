6a21afdcfa67f04b22f82b2c7a676dc621ed2b7d
MarioASG <mariosanhuezag@hotmail.com>
2023-01-28 23:34:24 -0300

    Backend

--- src/resources/users/routes/users.routes.js
+++ src/resources/users/routes/users.routes.js
@@ -4,7 +4,7 @@ import {getProfile} from '../controllers/users.controllers.js';
 
 const usersRouter = Router();
 
-const baseURI = 'users';
+const baseURI = '/users';
 //Se define la ruta que entregará la info del perfil del usuario
 usersRouter.get(`${baseURI}/profile`, verifyToken, getProfile);//Se valida el token
 //Si el token es válido, en la request se agregan los datos del usuario
